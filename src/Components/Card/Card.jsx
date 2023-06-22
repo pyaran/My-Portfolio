4c8982feef10fb9a33cf8eb10ddc4e5b966451eb
Santiago Jimenez Montilla <santiagojimenezmontilla@gmail.com>
2023-06-21 22:42:08 -0300

    cambio de colores

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import image1 from "./CardImgs/foto7.jpg";  
+import image1 from "./CardImgs/icon.png";  
 import "./Card.css"; 
 
 function NewCard (){
@@ -12,15 +12,13 @@ function NewCard (){
                     </div>
                     <div className="col-md-8">
                         <div className="card-body text-center">
-                            <h1 className="card-title">sobre mi</h1>
+                            <h1 className="card-title">SOBRE MI</h1>
                             <p className="paragraph">
-                            Soy un apasionado desarrollador front-end con experiencia en el stack MERN, listo para impulsar proyectos innovadores y cautivar a los usuarios con interfaces atractivas y funcionales.
+                            Soy un desarrollador front-end con experiencia en el stack MERN, listo para impulsar proyectos innovadores con interfaces atractivas y funcionales.
                             </p>
+                            
                             <p className="paragraph">
-                            Mi enfoque se centra en crear experiencias web impactantes y accesibles para todos los usuarios. Estoy familiarizado/a con la metodología ágil de Scrum, lo que me permite colaborar eficientemente en equipos, adaptarme rápidamente a los cambios y entregar resultados de calidad en cada iteración. Además, mi experiencia en diseño responsive me permite asegurar que las aplicaciones que desarrollo se vean y funcionen de manera óptima en todos los dispositivos y tamaños de pantalla.
-                            </p>
-                            <p className="paragraph">
-                            Soy un entusiasta del aprendizaje constante y siempre estoy buscando nuevos conocimientos para llevar mis habilidades al siguiente nivel. Me encanta explorar las últimas tendencias y tecnologías, lo cual me permite ofrecer soluciones innovadoras y mantenerme al tanto de las mejores prácticas en el desarrollo front-end. Estoy emocionado/a de enfrentar nuevos desafíos y aplicar mi entusiasmo por el crecimiento profesional en cada proyecto en el que participo.
+                            Soy un entusiasta del aprendizaje constante y siempre estoy buscando nuevos conocimientos para llevar mis habilidades al siguiente nivel. Estoy emocionado/a de enfrentar nuevos desafíos y aplicar mi entusiasmo por el crecimiento profesional en cada proyecto en el que participo.
                             </p>
                         </div>
                     </div>
--- src/Components/Presentation/Presentation.jsx
+++ src/Components/Presentation/Presentation.jsx
@@ -2,18 +2,24 @@ import "./Presentation.css"
 function NewPresentation(){
     
     return(
-        <div className="presentation container">
-            <h2 className="Hola">Hola, mi nombre es</h2>
-            <br />
-            <h1 className="nombre">Santiago Jimenez Montilla</h1>
-            <hr />
-            <i className="bi bi-keyboard-fill keyboard">   Desarrollador Front-end</i>
-            <br />
-            <i className="bi bi-geo-alt-fill pin">   San Miguel De Tucuman - Argentina</i>
-            <br />
-            <i className="bi bi-envelope mail">   santiagojimenezmontilla@gmail.com</i>
-            <br />
-            <i className="bi bi-telephone phone">   0381-5127458</i>
+        <div className="presentation">
+            <div className="container">
+                <h2 className="Hola">Hola, mi nombre es</h2>
+                <br />
+                <h1 className="nombre">Santiago Jimenez Montilla</h1>
+                <hr />
+                <i className="bi bi-keyboard-fill keyboard">   Desarrollador Front-end</i>
+                <br />
+                <br />
+                <i className="bi bi-geo-alt-fill pin">   San Miguel De Tucuman - Argentina</i>
+                <br />
+                <br />
+                <i className="bi bi-envelope mail">   santiagojimenezmontilla@gmail.com</i>
+                <br />
+                <br />
+                <i className="bi bi-telephone phone">   0381-5127458</i>
+                <hr />
+            </div>
         </div>
     )
 }
