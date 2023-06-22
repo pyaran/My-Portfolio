efbae1c8e6346d3b0accc1fb7f25ca2b1dc2884f
Santiago Jimenez Montilla <santiagojimenezmontilla@gmail.com>
2023-06-22 14:23:51 -0300

    add noreferrer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,11 @@ function NewFooter (){
         <div className="footer ">
             <div className="redes">
                 <div className="Linkedin">
-                    <a href="https://www.linkedin.com/in/sjmontilla/" target="_blank"><i className="bi bi-linkedin linkedin text-primary"></i>
+                    <a href="https://www.linkedin.com/in/sjmontilla/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin linkedin text-primary"></i>
                     </a>
                 </div>
                 <div className="Whatsapp" >
-                <a href="https://wa.me/3815127458" target="_blank"><i className="bi bi-whatsapp whatsapp text-success" name="whatsapp" ></i></a>
+                <a href="https://wa.me/3815127458" target="_blank" rel="noreferrer"><i className="bi bi-whatsapp whatsapp text-success" name="whatsapp" ></i></a>
                 
                 </div>
             </div>
