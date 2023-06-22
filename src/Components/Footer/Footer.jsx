401efd3ae4c37cf4c6806129b2745bb8877994c8
Santiago Jimenez Montilla <santiagojimenezmontilla@gmail.com>
2023-06-22 16:49:19 -0300

    github agregado al footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,15 +5,15 @@ function NewFooter (){
         <div className="footer ">
             <div className="redes">
                 <div className="Linkedin">
-                    <a href="https://www.linkedin.com/in/sjmontilla/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin linkedin text-primary"></i>
-                    </a>
+                    <a href="https://www.linkedin.com/in/sjmontilla/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin linkedin text-primary"></i></a>
                 </div>
                 <div className="Whatsapp" >
-                <a href="https://wa.me/3815127458" target="_blank" rel="noreferrer"><i className="bi bi-whatsapp whatsapp text-success" name="whatsapp" ></i></a>
-                
+                    <a href="https://wa.me/3815127458" target="_blank" rel="noreferrer"><i className="bi bi-whatsapp whatsapp text-success" name="whatsapp" ></i></a>
+                </div>
+                <div className="Github" >
+                    <a href="https://github.com/pyaran" target="_blank" rel="noreferrer"><i class="bi bi-github"></i></a>
                 </div>
             </div>
-                
         </div>
     )
 }
